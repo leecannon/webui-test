b45c2d5e07d30203e1d22eea70a179f3c49339c4
Lee Cannon <leecannon@leecannon.xyz>
2023-03-18 18:27:49 +0000

    add webui

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,14 @@
 const std = @import("std");
+const webui = @cImport({
+    @cInclude("webui.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const win = webui.webui_new_window();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const shown = webui.webui_show(win, "<html>Hello!</html>", webui.webui.browser.any);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("{}", .{shown}); // this is false and I don't know why
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    webui.webui_wait();
 }
